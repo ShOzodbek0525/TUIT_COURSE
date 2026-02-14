e53987ebe0229243c464d3dc1e2514b0cf612e60
ShOzodbek0525 <asalomdunyo@gmail.com>
2026-02-14 10:59:34 +0500

    Add humans and xodim classes with print methods

---
+++ N.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+class humans
+{
+private:
+  string nomi;
+  string yoshi;
+public:
+  humans(string nomi, string yoshi) :nomi(nomi), yoshi(yoshi) {}
+  void print_h()
+  {
+    cout << "Nomi=" << nomi << "Yoshi=" << yoshi << endl;
+  }
+};
+
+class xodim : public humans
+{
+private:
+  string mutaxasisligi;
+  double maoshi;
+public:
+  xodim(string mutaxasisligi, double maoshi, string nomi, string yoshi) :mutaxasisligi(mutaxasisligi), maoshi(maoshi), humans(nomi, yoshi) {}
+  void print()
+  {
+    print_h();
+    cout << "Mutaxasisligi=" << mutaxasisligi << "Maoshi=" << maoshi << endl;
+
+  }
+
+};
+int main() 
+{
+  xodim x("Shifokor", 2568974, "Alisa", "29");
+  x.print();
+
+  
+}
